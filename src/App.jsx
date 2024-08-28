aa6196cd6be950e25112ec9ea6db38eb83ab0919
hsz <santinogz131@gmail.com>
2024-08-28 10:51:22 -0300

    Se cambio el retorno del promedio del net income ya que tenia q usar un useState para retornarlo al App, por lo que directamente lo imprimo en el Table jsx desp de llamar a la funcion para hacer el calculo allí mismo

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import Table from './components/Table';
 
+
 const App = () => {
   let averageIncome = 0;
 
   const calculateAverage = (netIncomes) => {
     const totalIncome = netIncomes.reduce((acc, curr) => acc + curr.income, 0);
     averageIncome = totalIncome / netIncomes.length;
+    return averageIncome;
   };
 
   return (
     <div>
       <h1>Net Incomes by Brand</h1>
       <Table onCalculateAverage={calculateAverage} />
-      <p>Average Net Income: {averageIncome.toFixed(2)}</p>
     </div>
   );
 };
 
 export default App;
-
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -7,33 +7,33 @@ const Table = ({ onCalculateAverage }) => {
         { brand: 'KFC', income: 1098463 }
     ];
 
-    // Llamar a la función para calcular el promedio
-    onCalculateAverage(netIncomes);
+    const averageIncome = onCalculateAverage(netIncomes);
 
     return (
-        <table border="1">
-            <thead>
-                <tr>
-                    <th>Brand</th>
-                    <th>Net Income</th>
-                </tr>
-            </thead>
-            <tbody>
-                {netIncomes.map((income, index) => (
-                    <tr key={index}>
-                        <td>{income.brand}</td>
-                        <td>{income.income}</td>
+        <div>
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>Brand</th>
+                        <th>Net Income</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {netIncomes.map((income, index) => (
+                        <tr key={index}>
+                            <td>{income.brand}</td>
+                            <td>{income.income}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <p>Average Net Income: {averageIncome.toFixed(2)}</p>
+        </div>
     );
 };
 
-// Validación de props con lo aprendido hoy 28/08 que es el isRequired
 Table.propTypes = {
     onCalculateAverage: PropTypes.func.isRequired,
 };
 
 export default Table;
-
